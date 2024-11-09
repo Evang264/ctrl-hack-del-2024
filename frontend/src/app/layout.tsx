ccbfad6e0bc5c3add74deb7c6751659f1cf5c4d7
Lucas Kim <lucaskamadakim@gmail.com>
2024-11-09 16:54:07 -0500

    Fix merge issues

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,22 +1,9 @@
 "use client";
 
-import type { Metadata } from "next";
-import localFont from "next/font/local";
 import "./globals.css";
 import Navbar from "@/components/navbar";
 import clsx from "clsx";
 import { useState } from "react";
-
-const geistSans = localFont({
-  src: "./fonts/GeistVF.woff",
-  variable: "--font-geist-sans",
-  weight: "100 900",
-});
-const geistMono = localFont({
-  src: "./fonts/GeistMonoVF.woff",
-  variable: "--font-geist-mono",
-  weight: "100 900",
-});
 
 
 export default function RootLayout({
